67cbe0ba7bbd4e8bf3ffffc4b66915a68d3dc554
Eugene <slesh.eugene93@gmail.com>
2018-02-14 05:32:24 +0300

    database name was removed from insert queries

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -48,8 +48,8 @@ CREATE TABLE `wallet` (
   UNIQUE KEY `UK_wallet_address` (`address`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO monominer.wallet (id, address, balance) VALUES (1, '3J85FLJrjyqz8Dsj5F9pJ9kkQkqJsByt15', 0);
-INSERT INTO monominer.role (id, name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO monominer.role (id, name) VALUES (2, 'ROLE_USER');
-INSERT INTO monominer.application_user (id, password, username) VALUES (1, '$2a$16$c8m4JZESnrkb/laxILUjJOQ6SexRA//ltknKvL4C8rFpaOgwhkCsK', 'admin');
-INSERT INTO monominer.application_user_roles (application_user_id, roles_id) VALUES (1, 1);
+INSERT INTO wallet (id, address, balance) VALUES (1, '3J85FLJrjyqz8Dsj5F9pJ9kkQkqJsByt15', 0);
+INSERT INTO role (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO role (id, name) VALUES (2, 'ROLE_USER');
+INSERT INTO application_user (id, password, username) VALUES (1, '$2a$16$c8m4JZESnrkb/laxILUjJOQ6SexRA//ltknKvL4C8rFpaOgwhkCsK', 'admin');
+INSERT INTO application_user_roles (application_user_id, roles_id) VALUES (1, 1);
