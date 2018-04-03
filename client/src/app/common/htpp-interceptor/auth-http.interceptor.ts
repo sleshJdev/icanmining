bd15c2b453bc56dd1062cafb247575d4e7710a9f
Eugene <slesh.eugene93@gmail.com>
2018-04-03 04:03:03 +0300

    client deps were upgraded

--- client/src/app/common/htpp-interceptor/auth-http.interceptor.ts
+++ client/src/app/common/htpp-interceptor/auth-http.interceptor.ts
@@ -1,8 +1,8 @@
 import {Injectable, Injector} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 
-import {Observable} from 'rxjs/Observable';
 import {AuthService} from '../auth.service';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
